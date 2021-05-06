dcb420093fd6acd4e622e7a209d7659d4af1f1ca
prayaagmahajan <64199797+prayaagmahajan@users.noreply.github.com>
2021-05-06 20:02:59 +0530

    Update router.py

--- mypizza_app/pizza/router.py
+++ mypizza_app/pizza/router.py
@@ -1,4 +1,6 @@
 from rest_framework.routers import DefaultRouter
-from .views import PizzaViewset
+from .views import PizzaViewset,PizzaSizeViewset,PizzaToppingsViewset
 router = DefaultRouter()
 router.register(r'pizza', PizzaViewset)
+router.register(r'pizzasize', PizzaSizeViewset)
+router.register(r'pizzatopping', PizzaToppingsViewset)
